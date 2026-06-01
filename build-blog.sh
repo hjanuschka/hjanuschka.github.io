1a5f3b40097bc4c68133ab662ea2ff3b9d0b328d
Helmut Januschka <helmut@januschka.com>
2026-06-01 22:51:58 +0200

    Remove smooth scroll - was breaking anchor links

--- build-blog.sh
+++ build-blog.sh
@@ -1230,19 +1230,7 @@ $content
 
     palette.querySelector('.command-palette-backdrop').addEventListener('click', closePalette);
 
-    // Smooth scrolling for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-          target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-          });
-        }
-      });
-    });
+    
   </script>
   
   <!-- Prism.js for syntax highlighting -->
